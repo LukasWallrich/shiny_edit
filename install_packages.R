7662b034f46e5ffea118b39c90492d318ea5618e
Lukas Wallrich <lukas.wallrich@gmail.com>
2022-10-27 17:52:58 +0100

    Clean up

--- install_packages.R
+++
@@ -1,4 +0,0 @@
-install.packages("shiny", repos='https://cloud.r-project.org/')
-install.packages("dplyr", repos='https://cloud.r-project.org/')
-install.packages("stringr", repos='https://cloud.r-project.org/')
-install.packages("git2r", repos='https://cloud.r-project.org/')
