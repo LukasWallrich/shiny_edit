b7b41a5a79631c900ec55b9504740591f00204a9
Lukas Wallrich <lukas.wallrich@gmail.com>
2022-10-27 16:06:34 +0100

    Fix mirror

--- install_packages.R
+++ install_packages.R
@@ -1,3 +1,4 @@
-install.packages("shiny")
-install.packages("dplyr")
-install.packages("stringr")
+install.packages("shiny", repos='https://cloud.r-project.org/')
+install.packages("dplyr", repos='https://cloud.r-project.org/')
+install.packages("stringr", repos='https://cloud.r-project.org/')
+install.packages("git2r", repos='https://cloud.r-project.org/')
